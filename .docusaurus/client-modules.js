6b90713eefe993d9b689d21282c4ef534f17a232
mina.barry@epitech.eu <mina.barry@epitech.eu@epitech.eu>
2021-01-16 14:44:36 -0800

    fix(docs): remove .docusurus folder

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/Users/bibo/delivery/EIP/c3pm/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/bibo/delivery/EIP/c3pm/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/bibo/delivery/EIP/c3pm/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/bibo/delivery/EIP/c3pm/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/bibo/delivery/EIP/c3pm/docs/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-  require("/Users/bibo/delivery/EIP/c3pm/docs/src/css/custom.css"),
-];
